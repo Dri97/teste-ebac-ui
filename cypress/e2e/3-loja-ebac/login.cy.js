5963d3699913ed54cae5e765dff9b5fb6e503afd
Dri97 <145166422+Dri97@users.noreply.github.com>
2024-02-06 19:34:52 -0300

    Update login.cy.js

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,15 +1,1 @@
-///<reference types="cypress"/>
 
-const { should } = require("chai")
-
-describe('Funcionalidade: Login',() => { 
-it('Deve fazer login com sucesso', () => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-cy.get('#username').type('adrielekazoku@gmail.com')
-cy.get('#password').type('adrielle123')
-cy.get('.woocommerce-from > .button').click()
-
-cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, adrielekazoku (nao é adrielekazoku? Sair)')
-})
-
-})         

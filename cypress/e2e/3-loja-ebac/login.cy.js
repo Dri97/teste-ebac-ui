99ef5e2362809ca38340036ee95927ab425e0937
Dri97 <145166422+Dri97@users.noreply.github.com>
2024-02-06 20:36:33 -0300

    Update login.cy.js

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -7,20 +7,18 @@ beforeEach(() => {
 });
 
 afterEach(() => {
-    cy.screenshot()
-    
+    cy.screenshot() 
 });
-
-   it('Deve fazer login com sucesso', () => {
+ 
+it('Deve fazer login com sucesso', () => {
     cy.get('#username').type('adrielekazoku@gmail.com')
     cy.get('#password').type('adrielle123')
     cy.get('.woocommerce-from > .button').click()
     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, adrielekazoku (nao é adrielekazoku? Sair)')
-
-   });
+});
 
  it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
-    cy.get('#username').type('adrielllekazoku@gmail.com')
+    cy.get('#username').type('adriele.kazoku@gmail.com')
     cy.get('#password').type('adrielle123')
     cy.get('.woocommerce-from > .button').click()
     cy.get('.woocommerce-error').should('exist')
@@ -28,7 +26,7 @@ afterEach(() => {
 
  it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
     cy.get('#username').type('adrielekazoku@gmail.com')
-    cy.get('#password').type('adrielle123456')
+    cy.get('#password').type('adriele456')
     cy.get('.woocommerce-from > .button').click()
     cy.get('.woocommerce-error').should('contain' , 'Erro: a senha fornecida para o e-mail adrielekazoku@gmail.com está incorreta. Perdeu a senha?')
     cy.get('.woocommerce-error').should('exist')
